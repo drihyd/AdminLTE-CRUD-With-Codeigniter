1cb78af6ab29375a4fe40e4f111a7b2d5af718fe
drihyd <techsupport@deepredink.com>
2020-06-29 12:34:05 +0530

    CMS changes

--- application/views/customers/home.php
+++ application/views/customers/home.php
@@ -5,14 +5,17 @@
 <div class="box">
   <div class="box-header">
     <div class="col-md-6" style="padding: 0;">
-        <button class="form-control btn btn-primary" data-toggle="modal" data-target="#tambah-pegawai"><i class="glyphicon glyphicon-plus-sign"></i> Tambah Data</button>
+        <button class="form-control btn btn-primary" data-toggle="modal" data-target="#tambah-pegawai"><i class="glyphicon glyphicon-plus-sign"></i> Add Data</button>
     </div>
+	<!--
     <div class="col-md-3">
         <a href="<?php echo base_url('Pegawai/export'); ?>" class="form-control btn btn-default"><i class="glyphicon glyphicon glyphicon-floppy-save"></i> Export Data Excel</a>
     </div>
     <div class="col-md-3">
         <button class="form-control btn btn-default" data-toggle="modal" data-target="#import-pegawai"><i class="glyphicon glyphicon glyphicon-floppy-open"></i> Import Data Excel</button>
     </div>
+	
+	-->
   </div>
   <!-- /.box-header -->
   <div class="box-body">
@@ -23,8 +26,7 @@
           <th>Full Name</th>
           <th>Email</th>
           <th>Phone</th>
-          <th>Posisi</th>
-          <th style="text-align: center;">Aksi</th>
+          <th style="text-align: center;">Actions</th>
         </tr>
       </thead>
       <tbody id="data-pegawai">
--- assets/js/ajax.php
+++ assets/js/ajax.php
@@ -36,9 +36,8 @@
 	}
 
 	function tampilPegawai() {
-		$.get('<?php echo base_url('customers/tampil'); ?>', function(data) {
+		$.get('<?php echo base_url('customers/tampil'); ?>', function(data) {		
 			
-			alert(data);
 			MyTable.fnDestroy();
 			$('#data-pegawai').html(data);
 			refresh();
